66e981e101ca77062eb19e73b3d3fde4d19c1548
xahrah02-boop <xahrah02@gmail.com>
2026-06-04 16:58:53 +0100

    Show department and job role buttons prominently

--- public/app.js
+++ public/app.js
@@ -202,9 +202,10 @@ function renderUsers() {
 }
 
 function renderDepartments() {
-  return `<div class="split">
-    ${panel("Department master", (canManage() ? `<div class="toolbar"><button type="button" data-create-department>Add Department</button></div>` : "") + departmentTable())}
-    ${panel("Job roles", (canManage() ? `<div class="toolbar"><button type="button" data-create-job-role>Add Job Role</button></div>` : "") + jobRoleTable())}
+  return `${canManage() ? `<div class="toolbar page-actions"><button type="button" data-create-department>Add Department</button><button type="button" data-create-job-role>Add Job Role</button></div>` : ""}
+  <div class="split">
+    ${panel("Department master", departmentTable())}
+    ${panel("Job roles", jobRoleTable())}
   </div>`;
 }
 
